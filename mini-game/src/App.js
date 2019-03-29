f56fb8541679598c545a52b372a845262e37916a
tmullen <tmullen1991@gmail.com>
2019-03-29 13:29:22 -0600

    setup initial files

---
+++ mini-game/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Nav from "./components/nav/"
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+       <Nav />
+      </div>
+    );
+  }
+}
+
+export default App;
