979f5279b6785262ebb6c7e0697fcc36ff739ec1
tmullen <tmullen1991@gmail.com>
2019-03-31 01:02:03 -0600

    updated click functions still unfinished

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -3,47 +3,67 @@ import characters from "../../characters.json";
 import "./style.css";
 
 class App extends React.Component {
-  constructor (props){
+  constructor(props) {
     super(props);
-  
+
     this.state = {
-        characterList: characters,
-        wins: 0,
-        losses: 0,
-        active: false
-      };
+      characterList: characters,
+      score: 0,
+      topScore: 0,
+      active: ""
+    };
+  }
+
+  lost() {
+    this.setState({ score: 0 });
+  }
+
+  score() {
+    let score = this.state.score;
+    score = score + 1;
+    this.state.topScore > score ? this.setState({ topScore: score }) : console.log("");
+    this.setState({ score: score });
   }
 
   selectCard = name => {
-  
-    console.log(name);
+    this.state.active === name ? this.lost() : this.score();
+    this.setState({ active: name });
   };
 
   shuffleCards() {
-    var shuffle = require('shuffle-array')
-    let characterList = shuffle(characters)
-    this.setState({characterList})
+    var shuffle = require("shuffle-array");
+    let characterList = shuffle(characters);
+    this.setState({ characterList });
   }
 
   render() {
-    //let characterList = this.shuffleCards()
-    //console.log(characterList)
     return (
-      <div className="row text-center m-auto p-5" onClick={()=>this.shuffleCards()}>
-        {this.state.characterList.map(character =>
-      <div
-      className="card shadow rounded m-4"
-      onClick={() => this.selectCard(character.name)}
-      key={character.id}
-    >
-      <div className="img-container">
-        <img className="card-img-top" alt={character.name} src={character.image} />
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">{character.name}</h5>
-      </div>
-    </div>
-        )}
+      <div>
+        <h1 className="text-center">Score: {this.state.score}</h1>
+        <h1 className="text-center">TopScore: {this.state.score}</h1>
+        <div
+          className="row text-center m-auto p-5"
+          onClick={() => this.shuffleCards()}
+        >
+          {this.state.characterList.map(character => (
+            <div
+              className="card shadow rounded m-4"
+              onClick={() => this.selectCard(character.name)}
+              key={character.id}
+            >
+              <div className="img-container">
+                <img
+                  className="card-img-top"
+                  alt={character.name}
+                  src={character.image}
+                />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">{character.name}</h5>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
